2fb92f3f21aac7fab117b2d1d1950477d1007f40
Yashwin Tapdiya <yashwintapdiya@gmail.com>
2024-01-17 02:12:05 +0530

    react router implemented

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header'
+
+const About = () => {
+  return (
+    <div>
+        <Header/>
+        <h1>About</h1>
+        <h1>Testing the Router Routes</h1>
+    </div>
+  )
+}
+
+export default About
