a87c98eef2ad6df71b47d5c84513eb754287f0ce
Yashwin Tapdiya <yashwintapdiya@gmail.com>
2024-01-18 03:03:04 +0530

    showing online status using custom hook

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import { LOGO_URL } from "../utils/constants";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 const Header = () => {
   const [loginButton, setloginButton] = useState("login");
-  console.log("rendered");
+  const onlineStatus = useOnlineStatus();
   return (
     <div className="header">
       <div className="logo-container">
@@ -11,6 +12,7 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
+          <li>Online Status: {onlineStatus ? "✅" : "⛔"}</li>
           <li>
             <Link to="/">Home</Link>
           </li>
---
+++ src/utils/useOnlineStatus.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+const useOnlineStatus = () => {
+  const [onlineStatus, setOnlineStatus] = useState(true);
+  useEffect(() => {
+    window.addEventListener("offline", (e) => {
+      setOnlineStatus(false);
+    });
+    window.addEventListener("online", (e) => {
+      setOnlineStatus(true);
+    });
+  }, []);
+  return onlineStatus;
+};
+
+export default useOnlineStatus;
